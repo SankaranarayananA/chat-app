deb6723cd300723c94eb1e4f162f388657bb7db1
Sankaranarayanan A <76698114+SankaranarayananA@users.noreply.github.com>
2023-08-15 15:00:45 +0530

    Revert "added pusher"
    This reverts commit 2e767486d4fa0933fe9acbcb6ad28b961a0aaa98.

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,10 +1,8 @@
 "use client";
-import { pusherClient } from "@/lib/pusher";
-import { toPusherKey } from "@/lib/utils";
 import axios from "axios";
 import { Check, UserPlus, X } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { FC, use, useEffect, useState } from "react";
+import { FC, useState } from "react";
 
 interface FriendRequestsProps {
   incomingFriendRequests: IncomingFriendRequest[];
@@ -19,25 +17,6 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(
     incomingFriendRequests
   );
-
-  useEffect(() => {
-    pusherClient.subscribe(
-      toPusherKey(`user:${sessionId}:incoming_friend_requests`)
-    );
-
-    const friendRequestHandler = (data: IncomingFriendRequest) => {};
-
-    pusherClient.bind("incoming_friend_requests", friendRequestHandler);
-
-    return () => {
-      pusherClient.unsubscribe(
-        toPusherKey(`user:${sessionId}:incoming_friend_requests`)
-      );
-
-      pusherClient.unbind("incoming_friend_requests", friendRequestHandler);
-    };
-  }, []);
-
   const acceptFriend = async (senderId: string) => {
     await axios.post("/api/friends/accept", { id: senderId });
 
